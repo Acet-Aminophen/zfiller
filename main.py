fee3eaf34bc429291d1967cd05947b5c8a650f5a
Acet-Aminophen <59086376+Acet-Aminophen@users.noreply.github.com>
2022-12-01 19:56:37 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from natsort import natsorted
+from collections import defaultdict
+import os
+
+file_path = "/target"
+file_type = ["jpg", "png", "webp"]
+name_dic = defaultdict(int)
+
+for file in os.walk(file_path):
+    files = file[2][:]
+    for idx, name in enumerate(natsorted(files)):
+        type_is = name.split(".")[-1]
+        if type_is not in file_type:
+            continue
+        org_path = file[0] + "/" + name
+        renamed = file[0] + "/" + str(name_dic[file[0]] + 1).zfill(5) + "." + type_is
+        os.rename(org_path, renamed)
+        name_dic[file[0]] += 1
+        print("IN")
+        print(org_path)
+        print("OUT")
+        print(renamed)
+
+print("CLEAR")
