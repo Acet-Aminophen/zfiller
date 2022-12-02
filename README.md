d08396151790aa406c76871f2ffeeadab8efbc1a
Acet-Aminophen <leeds06080@gmail.com>
2022-12-02 14:00:19 +0900

    FEAT : Add types jpeg, jfif, gif FEAT : ignore directories that include "@eaDir"

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Why ["1.png", "10.png", "2.png"] is sorted files?
+# Why ["1.png", "10.png", "2.png"] is sorted?
 - This Docker Image changes file names like "00001.png" if their type is png, jpg, webp.
 - You must set volume destination as "/target" in the container.
+- It is made for The Synology system. so It ignores "@eaDir" Directory.
 - Every changed log will be printed.
 - Thanks
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from collections import defaultdict
 import os
 
 file_path = "/target"
-file_type = ["jpg", "png", "webp"]
+file_type = ["jpg", "png", "webp", "gif", "jpeg", "jfif"]
 name_dic = defaultdict(int)
 
 for file in os.walk(file_path):
     files = file[2][:]
     for idx, name in enumerate(natsorted(files)):
+        if "@eaDir" in file[0]:
+            continue
         type_is = name.split(".")[-1]
         if type_is not in file_type:
             continue
