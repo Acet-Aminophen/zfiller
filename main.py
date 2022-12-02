3b04b22dbb3457c03df4edfd5c809a8731881e84
Acet-Aminophen <leeds06080@gmail.com>
2022-12-02 14:39:47 +0900

    FEAT : recovering

--- main.py
+++ main.py
@@ -4,7 +4,30 @@ import os
 
 file_path = "/target"
 file_type = ["jpg", "png", "webp", "gif", "jpeg", "jfif"]
+recover_file_location = file_path + "/do_recover.txt"
 name_dic = defaultdict(int)
+
+if os.path.isfile(recover_file_location):
+    with open(recover_file_location, 'r', encoding="utf-8") as f:
+        log_list = f.read().splitlines()
+        in_is = ""
+        for idx, line in enumerate(log_list):
+            if line == "IN":
+                in_is = log_list[idx + 1]
+            if line == "OUT":
+                out_is = log_list[idx + 1]
+                if "@eaDir" in in_is or "@eaDir" in out_is:
+                    continue
+                else:
+                    os.rename(out_is, in_is)
+                    print("IN")
+                    print(out_is)
+                    print("OUT")
+                    print(in_is)
+                if line == "CLEAR":
+                    break
+    print("RECOVER CLEARED")
+    exit(0)
 
 for file in os.walk(file_path):
     files = file[2][:]
@@ -24,3 +47,4 @@ for file in os.walk(file_path):
         print(renamed)
 
 print("CLEAR")
+exit(0)
